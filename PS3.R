71423a2482a54a959ebf857132ed0d68e8781ed4
JackPloshnick <36051309+JackPloshnick@users.noreply.github.com>
2018-02-09 15:24:44 -0600

    PS3 with documentation

--- PS3.R
+++ PS3.R
@@ -3,18 +3,18 @@
 #Part 1
 Monty_Hall <-  as.numeric(sample(1:3,1))
 
-class(Monty_Hall) <- "door"
+class(Monty_Hall) <- "door"  #makes S3 class called door
 
 class(Monty_Hall)
 
 
 #Part 2
 
-PlayGame <- function(x){
+PlayGame <- function(x){   #creates Generic function called Playgame
   UseMethod("PlayGame",x)
 }
 
-PlayGame.door <- function(x){
+PlayGame.door <- function(x){ #plays the game
   car_door = as.numeric(sample(1:3,1))
   if(Monty_Hall == car_door){
     print("Congrats")
@@ -28,7 +28,7 @@ PlayGame(Monty_Hall)
 
 #Do it in S4
 
-setClass(Class = "door",
+setClass(Class = "door(S4)", #creates class in door in S4
          representation = representation(
            door = "numeric"
          ),
@@ -36,30 +36,30 @@ setClass(Class = "door",
            door = c()
          )
 )
-new("door")
+new("door(S4)")
 
 
-setValidity("door", function(object){
+setValidity("door(S4)", function(object){ #ensures door is one number between 1-3
  test1 = (object@door ==1 | object@door ==2 | object@door == 3)
  test2 = (length(object@door) == 1)
  if(!test1 | !test2){return("@door not valid")}
 }
 )
 
-setMethod("initialize", "door", function(.Object, ...) {
+setMethod("initialize", "door(S4)", function(.Object, ...) { #initializes 
   value = callNextMethod()
   validObject(value)
   return(value)
 })
 
-new("door", door= 1)
+new("door(S4)", door= 1)
 
-setGeneric("PlayGame",
-           function(object="door") {
-             standardGeneric("PlayGame")
+setGeneric("PlayGame_S4", #sets generic function in S4
+           function(object="door(S4)") {
+             standardGeneric("PlayGame_S4")
            } )
 
-setMethod("PlayGame", "door",
+setMethod("PlayGame_S4", "door(S4)", #plays the game in S4
           function(object){
             car_door = as.numeric(sample(1:3,1))
             if(object@door == car_door){
@@ -70,8 +70,8 @@ setMethod("PlayGame", "door",
             
           } )
 
-myObject <- new("door", door= 3)
+Monty_Hall_S4 <- new("door(S4)", door= 3)
 
-PlayGame(myObject)
+PlayGame_S4(Monty_Hall_S4)
 
 
